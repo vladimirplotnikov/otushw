90d702cfb56a49a829fc7a6441946315941e4fe2
vladimirplotnikov <monokius@gmail.com>
2023-06-06 23:49:29 +0300

    v3 Generics

--- HelloOtus/src/main/java/com/github/vladimirplotnikov/Box.java
+++ HelloOtus/src/main/java/com/github/vladimirplotnikov/Box.java
@@ -5,22 +5,12 @@ import java.util.List;
 
 //Создайте класс Box, в который можно складывать фрукты.
 public class Box<T extends Fruit> {
-    private T obj;
-
-    public T getObj() {
-        return obj;
-    }
 
     public Box(T obj) {
-        this.obj = obj;
-    }
-
-    public void setObj(T obj) {
-        this.obj = obj;
     }
 
     // Для хранения фруктов внутри коробки используйте ArrayList;
-    List<Fruit> T = new ArrayList<>();
+    List<Fruit> fruits = new ArrayList<>();
     // Коробки условно сортируются по типу фрукта, поэтому в одну коробку нельзя сложить и яблоки,
     // и апельсины (или же если коробка создана с типом Fruit, то допустимо и такое).
     //Не забываем про метод добавления фрукта в коробку.
@@ -29,36 +19,36 @@ public class Box<T extends Fruit> {
     void addFruit (T fruit) {
         System.out.println("Я добавляю "+fruit.getClass().getSimpleName()+",который весит "+fruit.getWeight()+" "+
                 fruit.getClass().getSimpleName());
-        T.add(fruit);
+        fruits.add(fruit);
     }
    //Реализуйте в классе Box метод weight(), который высчитывает вес коробки
    // (например, из веса одного фрукта и их количества (вес фрукта задайте сами, единицы измерения не важны),
    // или может через суммирование, тут как считаете нужным);
     double weight() {
         double currentWeight=0;
-        for (Fruit i : T) {
-            currentWeight=currentWeight+i.getWeight();
+        for (Fruit i : fruits) {
+            currentWeight += i.getWeight();
         }
         return  currentWeight;
     }
     //Реализуйте в классе Box метод compare(), который позволяет сравнить текущую коробку с той,
     // которую подадут в compare() в качестве параметра.
     // true – если их массы равны, false в противоположном случае. Можно сравнивать коробки с яблоками и апельсинами;
-    boolean compare(Box box2){
-        return Math.abs(this.weight() - box2.weight()) < 0.001f;
+    boolean compare(Box box){
+        return Math.abs(this.weight() - box.weight()) < 0.001f;
     }
     //Реализуйте метод, который позволяет пересыпать фрукты из текущей коробки в другую.
     // Помним про сортировку фруктов: нельзя яблоки высыпать в коробку с апельсинами.
     // Соответственно, в текущей коробке фруктов не остается, а в другую перекидываются объекты, которые были в первой;
-    boolean resort(Box box2){
+    boolean resort(Box box){
         //пустую коробку нет смысла пересыпать в другую, будем считать, что пересыпали в таком случае
-        if (this.T == null) {return true;}
-        if (this.T.isEmpty()) {return true;}
+        if (this.fruits == null) {return true;}
+        if (this.fruits.isEmpty()) {return true;}
         //проверим совместимость коробок
         Fruit box2fruit = null;
         //проверки пройдены, пересыпаем
-        for (Fruit i:T) {box2.addFruit(i);}
-            this.T.clear();
+        for (Fruit i: fruits) {box.addFruit(i);}
+            this.fruits.clear();
             return true;
     }
 }
